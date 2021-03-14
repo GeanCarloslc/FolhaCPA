8c454084fd5c0736405af4d6ff06944b9f3261e3
geancarloslc <geancarloslc@icloud.com>
2021-03-14 15:23:01 -0300

    Projeto Integrador Estacio.

---
+++ app/src/main/java/com/example/estacio/CadastrarAlunos.java
@@ -0,0 +1,14 @@
+package com.example.estacio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastrarAlunos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_alunos);
+    }
+}
---
+++ app/src/main/java/com/example/estacio/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.estacio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void cadastrarUsuario( View view ) {
+
+        Intent it = new Intent( MainActivity.this, CadastrarAlunos.class );
+        startActivity(it);
+
+
+    }
+}
