27e00cb0fc5ceeafcebc54a6f60a232091e55f45
geancarloslc <geancarloslc@icloud.com>
2021-03-17 20:28:50 -0300

    Teste

--- app/src/main/java/dominio/repositorio/GnrAlunoRepositorio.java
+++ app/src/main/java/dominio/repositorio/GnrAlunoRepositorio.java
@@ -14,14 +14,13 @@ public class GnrAlunoRepositorio {
     }
 
     public void inserir(GnrAluno gnrAluno){
-
+        //teste
         ContentValues contentValues = new ContentValues();
         contentValues.put("MATRICULA_GNR_ALUNO", gnrAluno.getMatriculaGnrAluno());
         contentValues.put("NOME_GNR_ALUNO", gnrAluno.getNomeGnrAluno());
         contentValues.put("LOGIN_GNR_ALUNO", gnrAluno.getLoginGnrAluno());
         contentValues.put("EMAIL_GNR_ALUNO", gnrAluno.getEmailGnrAluno());
         contentValues.put("PERIODO_GNR_ALUNO", gnrAluno.getPeriodoGnrAluno());
-
         conexao.insertOrThrow("GNR_ALUNO", null, contentValues);
 
     }
